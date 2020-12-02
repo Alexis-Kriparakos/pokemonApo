d358ae7ca7cad07012e6f2e9b8fe94152dff3ed4
Alexis Kriparakos <alexis.hennaut@gmail.com>
2020-12-03 00:01:17 +0200

    feat: add styling to pokemon cards

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,7 +1,11 @@
-export default function PokemonCard({pokemon,pokeImg}) {
+import styles from './PokemonCard.module.css';
+
+export default function PokemonCard({pokemon}) {
     return (
-        <div>
-           {pokemon.name}
+        <div className={styles.pokemonCard}>
+            <img className={styles.pokemonImage} src={pokemon.image} alt={pokemon.name}/>
+           <p className={styles.pokemonName}>{pokemon.name}</p>
+           <button className={styles.addPokemon}>Add to Team</button>
         </div>
     )
 }
