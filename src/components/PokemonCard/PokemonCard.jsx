e9cf5f19e52a609e5d7c46aaa3adcf37cc26fbbe
Alexis Kriparakos <alexis.hennaut@gmail.com>
2020-12-24 13:42:21 +0200

    tc: rempve take turns logic

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -6,16 +6,13 @@ export default function PokemonCard({pokemon}) {
     const image = get(pokemon, 'sprites.other.dream_world.front_default');
 
     function onClickAddToTeam(poke) {
-        const trainer1Turn = Trainer1Turn.getValue();
-        if(trainer1Turn){
-            const team1 = Trainer1Team.getValue();
-            if(team1.length < 5) {
-                Trainer1Team.update([...team1,poke]);
-                Trainer1Turn.update(false);
-                return;
-            }
-        }           
-         const team2 = Trainer2Team.getValue();
+        const team1 = Trainer1Team.getValue();
+        if(team1.length < 5) {
+            Trainer1Team.update([...team1,poke]);
+            Trainer1Turn.update(false);
+            return;
+        }
+        const team2 = Trainer2Team.getValue();
         if(team2.length < 5) {
             Trainer2Team.update([...team2,poke]);
             Trainer1Turn.update(true);
@@ -23,12 +20,11 @@ export default function PokemonCard({pokemon}) {
     }
 
     function onClickRemoveFromTeam(poke) {
-        const trainer1Turn = Trainer1Turn.getValue();
-        if(trainer1Turn){
-            const team1 = Trainer1Team.getValue();
-            const newTeam1 = team1.filter(p => p.id !== poke.id);
-            Trainer1Team.update(newTeam1);
-            return;
+        const team1 = Trainer1Team.getValue();
+        if(team1.length < 5) {
+        const newTeam1 = team1.filter(p => p.id !== poke.id);
+        Trainer1Team.update(newTeam1);
+        return;
         }
         const team2 = Trainer2Team.getValue();
         const newTeam2 = team2.filter(p=> p.id !== poke.id);
